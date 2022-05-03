d76f6203b88948ea41dc1c779d56c7a71428805f
Trevor <trevorfiler03@gmail.com>
2022-05-03 11:29:53 -0400

    Added build for deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -36,10 +37,6 @@ let notes = [
     important: true  
   }
 ]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) =>  {
   response.json(notes)
